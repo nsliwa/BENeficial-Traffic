b4bf97ef01a6cf913a5b387df09010f5a49192a8
nsliwa <nsliwa@smu.edu>
2015-02-05 13:15:23 -0600

    Incident List text fields fixed
    descriptor scrolls, and text fields are not editable

---
+++ B.E.N.-Lab1/SettingsTableViewController.m
@@ -0,0 +1,13 @@
+//
+//  SettingsTableViewController.m
+//  B.E.N.-Lab1
+//
+//  Created by ch484-mac4 on 2/5/15.
+//  Copyright (c) 2015 Team B.E.N. All rights reserved.
+//
+
+#import "SettingsTableViewController.h"
+
+@implementation SettingsTableViewController
+
+@end
